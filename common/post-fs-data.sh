9f4c1ac1965eecd2213ec715180d3b9b3c76269e
androidexpert35 <anty.cirielli.ac@gmail.com>
2017-03-26 19:02:19 +0200

    Inital module Release
    first release of this Magisk module

---
+++ common/post-fs-data.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in post-fs-data mode
+# Enable Hi_performance mode of your wcd9xx DAC
+echo 1 > /sys/module/snd_soc_wcd9*/parameters/high_perf_mode
