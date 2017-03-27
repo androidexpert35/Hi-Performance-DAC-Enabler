387c48c9d209c444c1f55cc638119ccb7b66a8c1
androidexpert35 <anty.cirielli.ac@gmail.com>
2017-03-27 11:37:52 +0200

    Fixed Execution on SnapDragon 805+ devices

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,4 +6,7 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # Enable Hi_performance mode of your wcd9xx DAC
 su
-busybox echo "1" > /sys/module/snd_soc_wcd9*/parameters/high_perf_mode
+chmod 0777 $MODDir/post-fs-data.sh
+busbox sleep 7
+busybox echo "1" > /sys/module/snd_soc_wcd93*/parameters/high_perf_mode
+busybox echo "1" > /sys/module/snd_soc_wcd92*/parameters/high_perf_mode
